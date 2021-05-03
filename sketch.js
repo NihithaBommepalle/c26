74d7fba38f9c7fa079e405cd7efcd00efa8ddb49
NihithaBommepalle <nrbmichiganroyaloak@gmail.com>
2021-05-03 17:37:31 -0400

    new platform has been added

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var platform1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,10 +15,12 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+    
 
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    platform1=new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -49,6 +52,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+    
 
     box3.display();
     box4.display();
@@ -61,4 +65,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    platform1.display();
 }
